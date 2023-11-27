9e41173bb6b22023203916cd9750bfd7cd693786
Alejandro <alejoramvids@gmail.com>
2023-11-26 22:12:52 -0600

    Todo terminado

--- PAT_Parcial03/Ejercicio02.cpp
+++ PAT_Parcial03/Ejercicio02.cpp
@@ -1,6 +1,40 @@
 #include "Ejercicio02.h"
+#include <map>
 
 vector<string>* Ejercicio02::findRepeatedDnaSequences(Node<char>* head)
 {
-    return nullptr;
+    std::map<string, int> dnaSequences;
+    vector<string>* result = new vector<string>();
+    string sequence = "";
+    Node<char>* current = head;
+
+    for (int i = 0; i < 10 && current != nullptr; ++i)
+    {
+        sequence += current->value;
+        current = current->next;
+    }
+    if (sequence.length() < 10)
+    {
+        return result;
+    }
+    ++dnaSequences[sequence];
+
+    while (current != nullptr)
+    {
+        sequence.erase(sequence.begin());
+        sequence += current->value;
+        ++dnaSequences[sequence];
+        current = current->next;
+    }
+
+    for (auto it = dnaSequences.begin(); it != dnaSequences.end(); ++it)
+    {
+        if (it->second > 1)
+        {
+            result->push_back(it->first);
+        }
+    }
+
+    return result;
 }
+
--- PAT_Parcial03/Ejercicio03.cpp
+++ PAT_Parcial03/Ejercicio03.cpp
@@ -1,14 +1,45 @@
 #include "Ejercicio03.h"
 
-TimeMap::TimeMap()
+TimeMap::TimeMap() {}
+
+void TimeMap::set(const std::string& key, const std::string& value, int timestamp)
 {
+    map[key].push_back({ timestamp, value });
 }
 
-void TimeMap::set(string key, string value, int timestamp)
+std::string TimeMap::get(const std::string& key, int timestamp)
 {
+    if (map.find(key) == map.end())
+        return "";
+
+    const auto& values = map[key];
+
+    int t = values.size();
+    int m = 0;
+    int b = 0;
+
+    if (values[b].timestamp > timestamp)
+        return "";
+
+    if (values[t - 1].timestamp <= timestamp)
+        return values[t - 1].value;
+
+    while (b < t) {
+        m = (t + b) >> 1;
+
+        if (values[m].timestamp == timestamp)
+            return values[m].value;
+
+        if (values[m].timestamp < timestamp)
+            b = m + 1;
+        else
+            t = m;
+    }
+
+    return values[m].value;
 }
 
-string TimeMap::get(string key, int timestamp)
+TimeMap::~TimeMap()
 {
-	return "";
+    map.clear();
 }
